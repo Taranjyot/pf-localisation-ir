6de82317e91e76f7623cd5e168e09c422366f9dc
GrannySmith21 <bastien.barbe65@gmail.com>
2021-10-27 01:33:14 +0100

    Making hierarchical clustering faster with use of a matrix

--- src/pf_localisation/pf.py
+++ src/pf_localisation/pf.py
@@ -3,6 +3,7 @@ import math
 from geometry_msgs.msg import Pose, PoseArray, Quaternion, Point
 from . pf_base import PFLocaliserBase
 import rospy
+import numpy as np
 
 from . util import rotateQuaternion, getHeading
 from random import random, gauss
@@ -85,8 +86,7 @@ class PFLocaliser(PFLocaliserBase):
         #self.particlecloud.poses.sort(key= lambda p: self.sensor_model.get_weight(scan, p) )
         for p in self.particlecloud.poses:
             weight_data.append(self.sensor_model.get_weight(scan, p))
-            #print(self.sensor_model.get_weight(scan, p))
-        
+
         normalised_weight_data = self.normalise(weight_data)
         cdf = [normalised_weight_data[0]]
         
@@ -99,34 +99,23 @@ class PFLocaliser(PFLocaliserBase):
         threshold = random()* math.pow(desired_particles_num,-1)
         i=1
         new_particle_cloud = PoseArray()
-        #print('CDF',cdf)
-        
+
         for j in range(0, desired_particles_num):
             while(threshold > cdf[i]):
                 i+=1
 
-                # make sigma prop to the inverse of the weight, 
-            
-            #sigma = 1.1/weight_data[i]
+                # make sigma prop to the inverse of the weight,
+
             if weight_data[i] < 4:
                 #robot kidnapped/particle is far away from robot pos
                 sigma = 3
             else:
                 sigma = 1.1/weight_data[i]
             new_particle_cloud.poses.append(self.init_random_pose(self.particlecloud.poses[i],sigma))
-            
-            '''if j+1 == len(threshold):
-                threshold.append(threshold[j] + math.pow(desired_particles_num,-1))
-            else:
-                threshold[j+1] = threshold[j] + math.pow(desired_particles_num,-1)
-            '''
-            threshold = threshold + math.pow(desired_particles_num,-1)
-         #   print('Threshold', threshold,'cdf i', cdf[i])
-        #print(cloned_particles)
-        self.particlecloud = new_particle_cloud
 
-        #print(self.particlecloud.poses[0])
-        #input()
+            threshold = threshold + math.pow(desired_particles_num,-1)
+
+        self.particlecloud = new_particle_cloud
         
     def normalise(self, _l):
         l = _l.copy()
@@ -157,9 +146,16 @@ class PFLocaliser(PFLocaliserBase):
             | (geometry_msgs.msg.Pose) robot's estimated pose.
          """
 
-        timer = time.time()
-        # Implementing hierarchical clustering
+        # Implementing bottom-up hierarchical clustering
 
+        """
+        Computes the mean of a cluster
+        :Args:
+            | cluster ([geometry_msgs.msg.Pose]): an array of poses representing the cluster
+        :Return:
+            | (float, foat, int) the mean of the x and y coordonates of each particules within the cluster plus the
+            number of particules within the cluster.
+        """
         def calculate_mean(cluster):
             mean_x = 0
             mean_y = 0
@@ -171,42 +167,66 @@ class PFLocaliser(PFLocaliserBase):
             mean_y /= len_cluster
             return mean_x, mean_y, len_cluster
 
-        # Each pose is a cluster itself
-        cluster_list = [] # list of clusters, a cluster is a list of poses
-        cluster_data_list = [] # list of the means of the clusters
+        # Firstly, each pose is a cluster itself
+        cluster_list = [] # list of clusters, a cluster is a list of poses [particule_1, ..., particule_n]
+        cluster_data_list = [] # list of the means of the clusters [(mean_x, mean_y, len_cluster), ...]
         number_clusters = 0
+
+        # Filling the lists
         for p in self.particlecloud.poses:
             cluster_list.append([p])
             cluster_data_list.append((p.position.x, p.position.y, 1))
             number_clusters += 1
-        #print("Creation liste : " + str(time.time() - timer))
-        timer = time.time()
 
+        """
+        Matrix stocking the distances between each and every clusters
+        For n=5 clusters for instance:
+                    0          1          2          3          4
+        0  [       inf        inf        inf        inf        inf]
+        1  [0.75692557        inf        inf        inf        inf]
+        2  [1.21914767 1.11622158        inf        inf        inf]
+        3  [1.59767441 1.15018727 1.20443125        inf        inf]
+        4  [0.81266185 0.99565461 0.50851403 1.0390767         inf]
+        Where distance between cluster 2 and 1 is 1.11622158 units.
+        """
+        distance_matrix = np.empty(shape=(number_clusters,number_clusters))
+        # Initializing the matrix with the maximum float value which is handy for computing the lowest distance
+        distance_matrix.fill(float('inf'))
+        # Filling the matrix the distances between each and every clusters
+        for i in range(number_clusters):
+            for j in range(number_clusters):
+                if i > j:
+                    distance_matrix[i][j] = math.dist(cluster_data_list[i][:2], cluster_data_list[j][:2])
+
+        # We keep merging the two nearest clusters until with have only 2 of them or the shortest distance between
+        # them is long enough
         while number_clusters > 2:
-            lowest_distance = float('inf')
-            cluster1 = 0
-            cluster2 = 0
-            # Finding the two closest clusters
-            for i in range(number_clusters):
-                for j in range(number_clusters):
-                    if i > j:
-                        distance = math.dist(cluster_data_list[i][:2], cluster_data_list[j][:2])
-                        if distance < lowest_distance:
-                            lowest_distance = distance
-                            cluster1 = i
-                            cluster2 = j
 
-            if lowest_distance > 1:
+            # Finding the two closest clusters in the matrix
+            cluster_i = np.where(distance_matrix == np.amin(distance_matrix))[0][0]
+            cluster_j = np.where(distance_matrix == np.amin(distance_matrix))[1][0]
+
+            # If the distance between the two closest clusters is more than 1 unit we consider that our clusters are
+            # good enough so we end the while loop
+            if distance_matrix[cluster_i][cluster_j] > 1:
                 break
-            # Merging the two closest clusters
+
+            # We merge the two clusters together (by merging the cluster j into the cluster i
+            cluster_list[cluster_i].extend(cluster_list[cluster_j])
+            cluster_data_list[cluster_i] = calculate_mean(cluster_list[cluster_i])
+
+            # We computes the new distances between the new formed cluster and all the other clusters
+            for k in range(number_clusters):
+                if cluster_i > k:
+                    distance_matrix[cluster_i][k] = math.dist(cluster_data_list[cluster_i][:2], cluster_data_list[k][:2])
+                elif k > cluster_i:
+                    distance_matrix[k][cluster_i] = math.dist(cluster_data_list[cluster_i][:2], cluster_data_list[k][:2])
+
+            # We delete the cluster j since he is now in the cluster i
+            cluster_list.pop(cluster_j)
+            cluster_data_list.pop(cluster_j)
+            distance_matrix = np.delete(np.delete(distance_matrix, cluster_j, axis=0), cluster_j, axis=1)
             number_clusters -= 1
-            cluster_list[cluster1].extend(cluster_list[cluster2])
-            # Calculate new mean of the cluster
-            cluster_data_list[cluster1] = calculate_mean(cluster_list[cluster1])
-            cluster_list.pop(cluster2)
-            cluster_data_list.pop(cluster2)
-        #print("Boucle while : " + str(time.time() - timer))
-        timer = time.time()
 
         # Computes the tallest cluster
         tallest_cluster_len = -42
@@ -215,10 +235,8 @@ class PFLocaliser(PFLocaliserBase):
             if cluster_data_list[c][2] > tallest_cluster_len:
                 tallest_cluster_len = cluster_data_list[c][2]
                 tallest_cluster_index = c
-        #print("Plus grand cluster : " + str(time.time() - timer))
-        timer = time.time()
 
-
+        # Computes the estimate pose
         pose = Pose()
         p = Point()
         p.x = cluster_data_list[tallest_cluster_index][0]
@@ -231,21 +249,9 @@ class PFLocaliser(PFLocaliserBase):
             sum_wr+= c.orientation.w
         q = Quaternion()
         len_tallest_cluster = len(tallest_cluster)
-        #print(len_tallest_cluster)
         q.z = sum_zr/len_tallest_cluster
         q.w = sum_wr/len_tallest_cluster
         pose.position = p
         pose.orientation = q
-        #print("Calcul postion : " + str(time.time() - timer))
 
         return pose
-
-
-
-'''
-adding noise to new particles
-pose estimation - clustering
-pose orientation 
-decreasing variance for noise
-increasing efficency of update_particle function
-'''
