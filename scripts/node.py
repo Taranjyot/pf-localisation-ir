0f5f0d96e502e537211dad59594c561f4b2f59d1
Taranjyot Singh <jyotlive91@gmail.com>
2021-11-29 15:31:25 +0000

    Color Detection Implmentation

--- scripts/node.py
+++ scripts/node.py
@@ -9,7 +9,7 @@ pf.PFLocaliser() to do the localisation.
 import rospy
 import pf_localisation.pf
 from pf_localisation.util import *
-
+from sensor_msgs.msg import Image
 from geometry_msgs.msg import ( PoseStamped, PoseWithCovarianceStamped,
                                 PoseArray, Quaternion )
 from tf.msg import tfMessage
@@ -20,6 +20,9 @@ from threading import Lock
 
 import sys
 from copy import deepcopy
+from cv_bridge import CvBridge
+import cv2
+import numpy as np
 
 class ParticleFilterLocalisationNode(object):
     def __init__(self):
@@ -60,6 +63,7 @@ class ParticleFilterLocalisationNode(object):
                                                      self._odometry_callback,
                                                      queue_size=1)
 
+        self._seeker_camera_subscriber = rospy.Subscriber('/robot_0/image',Image, self._robot_detection, queue_size=1)
     def _initial_pose_callback(self, pose):
         """ called when RViz sends a user supplied initial pose estimate """
         self._particle_filter.set_initial_pose(pose)
@@ -80,7 +84,56 @@ class ParticleFilterLocalisationNode(object):
                 rospy.logwarn("Filter cycle overran timeslot")
                 rospy.loginfo("Odometry update: %fs"%t_odom)
                 rospy.loginfo("Particle update: %fs"%t_filter)
-    
+
+    def _robot_detection(self, ros_data):
+        #np_arr = np.fromstring(ros_data.data, np.uint8)
+        #image_np = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
+
+        rgb_image = CvBridge().imgmsg_to_cv2(ros_data, desired_encoding="passthrough")
+        ##image_np = cv2.cvtColor(np_arr, cv2.COLOR_GRAY2BGR)
+        ##image_np = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
+        # load image
+        ##img = cv2.imread(image_np)
+        # Convert to HSV
+
+        hsv = cv2.cvtColor(rgb_image, cv2.COLOR_BGR2HSV)
+        cv2.imwrite('Test_gray.jpg', rgb_image)
+
+        # define range wanted color in HSV
+        lower_val_red = np.array([0, 102, 0])
+        upper_val_red = np.array([52, 255, 255])
+
+        lower_val_purple = np.array([0, 188, 0])
+        upper_val_purple = np.array([156, 242, 255])
+
+        lower_val_green = np.array([52, 62, 0])
+        upper_val_green = np.array([110, 255, 255])
+
+        lower_val_blue = np.array([111, 50, 0])
+        upper_val_blue = np.array([130, 255, 255])
+
+        # Threshold the HSV image - any green color will show up as white
+        mask_red = cv2.inRange(hsv, lower_val_red, upper_val_red)
+        mask_purple = cv2.inRange(hsv, lower_val_purple, upper_val_purple)
+        mask_green = cv2.inRange(hsv, lower_val_green, upper_val_green)
+        mask_blue = cv2.inRange(hsv, lower_val_blue, upper_val_blue)
+        # if there are any white pixels on mask, sum will be > 0
+        hasred = np.sum(mask_red)
+        haspurple = np.sum(mask_purple)
+        hasgreen = np.sum(mask_green)
+        hasblue = np.sum(mask_blue)
+        if hasred > 0:
+            print('Red detected!')
+
+        if haspurple > 0:
+            print('Purple detected!')
+
+        if hasgreen > 0:
+            print('Green detected!')
+
+        if hasblue > 0:
+            print('Blue detected!')
+
     def _laser_callback(self, scan):
         """
         Laser received. Store a ref to the latest scan. If robot has moved
